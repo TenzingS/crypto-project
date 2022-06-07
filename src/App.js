3c286b9bbccbbccb60446c109ad563f13c0b4e8f
TenzingS <thesherpatenzing@gmail.com>
2022-06-06 23:50:17 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { ThemeProvider } from "./context/ThemeContext"
+import Navbar from "./components/Navbar"
+
+function App() {
+  return (
+    <ThemeProvider>
+      <Navbar />
+    </ThemeProvider>
+  );
+}
+
+export default App;
