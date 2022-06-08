3cf3aba1d56b0bf7393c70b0261b31dec9564fd7
TenzingS <thesherpatenzing@gmail.com>
2022-06-07 22:14:00 -0400

    routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import React from "react";
-import { ThemeProvider } from "./context/ThemeContext"
-import Navbar from "./components/Navbar"
+import { ThemeProvider } from "./context/ThemeContext";
+import Navbar from "./components/Navbar";
+import { Route, BrowserRouter as Router } from 'react-router-dom';
+import Home from './routes/Home';
+import SignIn from './routes/SignIn';
+import SignUp from './routes/SignUp';
+import Account from './routes/Account';
 
 function App() {
   return (
     <ThemeProvider>
       <Navbar />
+      <Router>
+        <Route path='/' element={<Home />} />
+        <Route path='/signin' element={<SignIn />} />
+        <Route path='/signup' element={<SignUp />} />
+        <Route path='/account' element={<Account />} />
+      </Router>
     </ThemeProvider>
   );
 }
