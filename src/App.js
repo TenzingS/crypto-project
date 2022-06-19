923b013b996a64389b9968554569bd9da637adf5
TenzingS <thesherpatenzing@gmail.com>
2022-06-19 00:01:22 -0400

    Api added

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect} from "react";
 import { ThemeProvider } from "./context/ThemeContext";
 import Navbar from "./components/Navbar";
 import { Route, BrowserRouter as Router } from 'react-router-dom';
@@ -6,13 +6,25 @@ import Home from './routes/Home';
 import SignIn from './routes/SignIn';
 import SignUp from './routes/SignUp';
 import Account from './routes/Account';
+import axios from "axios";
 
 function App() {
+  const [coins, setCoins] = useState([])
+
+  const url = 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=true';
+
+  useEffect(() => {
+    axios.get(url).then((response) => {
+      setCoins(response.data)
+      console.log(response.data)
+    })
+  }, [url])
+  
   return (
     <ThemeProvider>
       <Navbar />
       <Router>
-        <Route path='/' element={<Home />} />
+        <Route path='/' element={<Home coins={coins} />} />
         <Route path='/signin' element={<SignIn />} />
         <Route path='/signup' element={<SignUp />} />
         <Route path='/account' element={<Account />} />
--- src/components/CoinSearch.jsx
+++ src/components/CoinSearch.jsx
@@ -1,7 +1,47 @@
 import React from 'react'
+import {AiOutlineStar} from 'react-icons/ai'
 
-export const CoinSearch = () => {
+export const CoinSearch = ({coins}) => {
   return (
-    <div>'CoinSearch'</div>
+    <div>
+      <div>
+        <h1>Search Crypto</h1>
+        <form>
+          <input type="text" placeholder='Search a coin' />
+        </form>
+      </div>
+      
+      <table>
+        <thead>
+          <tr>
+            <th></th>
+            <th>#</th>
+            <th>Coin</th>
+            <th></th>
+            <th>Price</th>
+            <th>24h</th>
+            <th>24h Volume</th>
+            <th>Mkt</th>
+            <th>Last 7 Days</th>
+          </tr>
+        </thead>
+        <tbody>
+          {coins.map((coin) => (
+            <tr>
+              <td><AiOutlineStar /></td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+
   )
 }
